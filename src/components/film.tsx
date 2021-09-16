cc39d08344a42e2c73e6e35d7e4571e3c839651b
Ivan <duremar98@yandex.ru>
2021-09-16 21:50:31 +0300

    film component

--- src/components/film.tsx
+++ src/components/film.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { FC, useState, useEffect } from "react";
+import { useParams } from "react-router";
+import { useTypeSelector } from "../hooks/useTypeSelector";
 
-const Film = () => {
-  return <div>2233</div>;
+import { Content } from "../style/film_style";
+
+interface ParamTypes {
+  id: string;
+}
+
+const Film: FC = () => {
+  let { id } = useParams<ParamTypes>();
+  const { data } = useTypeSelector((state) => state.films);
+  const [film, setFilm] = useState<object[]>([{}]);
+
+  useEffect(() => {
+    setFilm(data.filter((item) => item.filmId.toString() === id));
+  }, [data, id]);
+
+  console.log("films", film[0]);
+
+  return (
+    <Content>
+      {" "}
+      {film[0] ? (
+        <div>
+          <p>{id}</p>
+          <p></p>
+        </div>
+      ) : (
+        <p>loading</p>
+      )}
+    </Content>
+  );
 };
 
 export default Film;
