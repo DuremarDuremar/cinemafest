c0a1b00d4c043082917860804455089fe2b6917a
Ivan <duremar98@yandex.ru>
2021-09-21 23:40:26 +0300

    new component film

--- src/components/film.tsx
+++ src/components/film.tsx
@@ -2,36 +2,70 @@ import React, { FC, useState, useEffect } from "react";
 import { useParams } from "react-router";
 import { useTypeSelector } from "../hooks/useTypeSelector";
 
-import { Content } from "../style/film_style";
+import {
+  Content,
+  Header,
+  Main,
+  Image,
+  Info,
+  InfoFilm,
+  InfoDirector,
+  Frames,
+} from "../style/film_style";
 
 interface ParamTypes {
   id: string;
 }
 
 interface FilmTypes {
-  [index: number]: { filmId: number; nameEn: string };
+  [index: number]: {
+    filmId: number;
+    posterUrlPreview: string;
+    nameEn?: string;
+    nameRu?: string;
+    countries: [{ country: string }];
+    year: number;
+  };
 }
 
 const Film: FC = () => {
   let { id } = useParams<ParamTypes>();
   const { data } = useTypeSelector((state) => state.films);
-  const [film, setFilm] = useState<FilmTypes>([]);
+  const [film, setFilm] = useState<FilmTypes | null>(null);
 
   useEffect(() => {
     if (data.length) {
       setFilm(data.filter((item) => item.filmId.toString() === id));
     }
   }, [data, id]);
-
-  console.log("films", film[0].nameEn);
+  if (film) {
+    console.log("films", film[0].countries);
+  }
 
   return (
     <Content>
       {film ? (
-        <div>
-          <p>{id}</p>
-          <p></p>
-        </div>
+        <>
+          <Header></Header>
+          <Main>
+            <Image>
+              <img src={film[0].posterUrlPreview} alt={film[0].nameEn} />
+            </Image>
+            <Info>
+              <InfoFilm>
+                <p>{film[0].nameRu}</p>
+                <p>{film[0].nameEn}</p>
+                <div>
+                  {film[0].countries.map((item, index) => (
+                    <span key={index}>{item.country}</span>
+                  ))}
+                </div>
+                <p>{film[0].year}</p>
+              </InfoFilm>
+              <InfoDirector></InfoDirector>
+            </Info>
+          </Main>
+        </>
       ) : (
         <p>loading</p>
       )}
--- src/style/film_style.tsx
+++ src/style/film_style.tsx
@@ -1,3 +1,11 @@
 import styled from "styled-components";
 
 export const Content = styled.div``;
+
+export const Header = styled.div``;
+export const Main = styled.div``;
+export const Image = styled.div``;
+export const Info = styled.div``;
+export const InfoFilm = styled.div``;
+export const InfoDirector = styled.div``;
+export const Frames = styled.div``;
